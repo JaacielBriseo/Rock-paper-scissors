1d21f5521c89a6939cfe68d1df75f7421b330a90
JaacielBriseo <jaacielbris23@gmail.com>
2022-12-12 11:20:11 -0700

    Header Layout mobile version

--- src/MainApp.tsx
+++ src/MainApp.tsx
@@ -1,6 +1,9 @@
+import { Header } from './components/Header';
 
 export const MainApp = () => {
-  return (
-    <div>MainApp</div>
-  )
-}
+	return (
+		<div>
+			<Header />
+		</div>
+	);
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
 			fontFamily: {
 				sans: ['Barlow Semi Condensed'],
 			},
+			backgroundImage: {
+				Triangle: 'url(assets/bg-triangle.svg)',
+			},
+			colors: {
+				DarkText: 'hsl(229, 25%, 31%)',
+				ScoreText: 'hsl(229, 64%, 46%)',
+				HeaderOutline: 'hsl(217, 16%, 45%)',
+			},
 		},
 	},
 	plugins: [],
